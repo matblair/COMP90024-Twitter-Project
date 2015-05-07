1ffecefdaecb76943916a2222eb9847a9fdd5fc0
Pheo <CJM1993@gmail.com>
2015-05-07 23:00:44 +1000

    'New' tweet grabbing code. The old one missed out MANY fields. All single core atm, I will port to multicore after REST routes are exposed.
    tweetCSVParser.py obsoleted temporarily... The old data is of little use?

--- TweetGrabberTagger/tweetGrabber.py
+++ TweetGrabberTagger/tweetGrabber.py
@@ -1,4 +1,30 @@
 # tweetGrabber.py
+# ~Jun Min (542339)
 
-if __name__ == '__main__':
-    
+import tweepy
+import json
+
+from tweetTagger import tweetTagger
+
+# API Key, Secret
+api_key="esfbRz6gUhGyckd2t7K7BLQeW"
+api_secret="A7GyPaNmpJsKgXXgR2qIgNCRhPN1KCvb0qJ3YfImPqHNevhTwx"
+
+# Longitude, Latitude
+lat = "29.424122"
+lng = "-98.493629"
+
+auth = tweepy.auth.AppAuthHandler(api_key, api_secret)
+
+def search_api(auth):
+
+    api = tweepy.API(auth)
+
+    radius = "50km"
+    geocode_str = lat + "," + lng + "," + radius
+    tweets = api.search("", geocode=geocode_str, count=1)
+    for tweet in tweets:
+        tagged_tweet = tweetTagger(tweet._json)
+        print(tagged_tweet.getJSONTaggedTweet())
+
+search_api(auth)
