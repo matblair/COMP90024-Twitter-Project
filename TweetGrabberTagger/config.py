598b83302d3f29ba80e13ec22d981b76ef8646e2
Pheo <CJM1993@gmail.com>
2015-05-10 01:55:13 +1000

    hehe :)

--- TweetGrabberTagger/config.py
+++ TweetGrabberTagger/config.py
@@ -3,12 +3,12 @@ class Config:
     '''Config Class, holds configs'''
 
     # Consumer Key/Secret
-    consumer_key="esfbRz6gUhGyckd2t7K7BLQeW"
-    consumer_secret="A7GyPaNmpJsKgXXgR2qIgNCRhPN1KCvb0qJ3YfImPqHNevhTwx"
+    consumer_key=""
+    consumer_secret=""
 
     # Access Token/Secret
-    access_token="2362162429-GYwJY4gknR5Gj6JHs9aSlP7hax8k3Z0fXK8PVwE"
-    access_token_secret="4kWkv1Yqpst0o7V7QaYMiusFZppAyCtOXg06AJ693o1WZ"
+    access_token=""
+    access_token_secret=""
 
     # Latitude / Longitude
     longitude = -98.493629
