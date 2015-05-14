25097a2afb6590c79f7ae2a1e8f2645139ebb3a1
Renlord <renlord.yang@gmail.com>
2015-05-15 02:27:53 +1000

    more work done.

--- client/src/js/controllers/fact.js
+++ client/src/js/controllers/fact.js
@@ -1,34 +1,37 @@
 app.controller("FactController", ['$scope', '$http', function($scope, $http) {
 
-		$scope.hashtagFetch = function() {
-			var req = {
-			 method: 'GET',
-			 url: "http://144.6.227.63:4500/hashtags/trending/",
-			 headers: {
-			   'Content-Type': undefined
-			 },
-			 data:{}
-			}
-			$http(req).
-				success(function(data, status, headers, config) {
-					$scope.tags = data.response;
-				}).
-				error(function(data, status, headers, config){ 
-					alert("http://144.6.227.63:4500/hashtags/trending NOT REACHABLE");
-				})
-		}
+	/* Variables Initialisation */
+	$scope.trendHash = {}
 
-		$scope.socialGraphFetch = function() {
-			$http.get("http://144.6.227.63:4500/users/").
-				success(function(data, status, headers, config) {
-					console.log(data)
-					$scope.socialGraph = data;
-				}).
-				error(function(data, status, headers, config) {
-					alert("http://144.6.227.63:4500/users/", status);
-				})
+	$scope.hashtagFetch = function() {
+		var req = {
+		 method: 'GET',
+		 url: "http://144.6.227.63:4500/hashtags/trending/",
+		 headers: {
+		   'Content-Type': undefined
+		 },
+		 data: $scope.trendHash
 		}
+		$http(req).
+			success(function(data, status, headers, config) {
+				$scope.tags = data.response;
+			}).
+			error(function(data, status, headers, config){ 
+				alert("http://144.6.227.63:4500/hashtags/trending NOT REACHABLE");
+			})
+	}
 
-		$scope.hashtagFetch()
-		$scope.socialGraphFetch()
-	}])
+	$scope.socialGraphFetch = function() {
+		$http.get("http://144.6.227.63:4500/users/").
+			success(function(data, status, headers, config) {
+				$scope.socialGraph = data;
+			}).
+			error(function(data, status, headers, config) {
+				alert("http://144.6.227.63:4500/users/", status);
+			})
+	}
+
+	/* Init Run - Bad way to do it... but wtv... */
+	$scope.hashtagFetch()
+	$scope.socialGraphFetch()
+}])
